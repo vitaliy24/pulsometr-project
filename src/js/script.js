ee3f377d038f331651c4e0a31a247788ed86b264
“Vitaliy” <example@gmail.com>
2020-01-30 20:45:58 +0600

    modal added, adopted. forms, button adopted for modal_media

--- src/js/script.js
+++ src/js/script.js
@@ -20,21 +20,6 @@ $(document).ready(function(){
       .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
   });
 
-/*   $('.catalog-item__link').each(function(i) {
-    $(this).on('click', function(e) {
-      e.preventDefault();
-      $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
-      $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
-    })
-  });
-
-  $('.catalog-item__back').each(function(i) {
-    $(this).on('click', function(e) {
-      e.preventDefault();
-      $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
-      $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
-    })
-  }); */
 
   function toggleSlide(item) {
     $(item).each(function(i) {
@@ -49,26 +34,20 @@ $(document).ready(function(){
   toggleSlide('.catalog-item__link');
   toggleSlide('.catalog-item__back');
 
+  // Modal
+
+  $('[data-modal=consultation]').on('click', function() {
+    $('.overlay, #consultation').fadeIn('slow');
+  });
+  $('.modal__close').on('click', function() {
+    $('.overlay, #consultation, #thanks, #order').fadeOut('slow');
+  });
+  
+  $('.button_mini').each(function(i) {
+    $(this).on('click', function() {
+      $('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text());
+      $('.overlay, #order').fadeIn('slow');
+    });
+  });
+
 });
-
-
-
-
-
-  /* const slider = tns({
-    container: '.carousel__inner',
-    items: 1,
-    slideBy: 'page',
-    autoplay: false,
-    controls: false,
-    nav: false
-  });
-
-  document.querySelector('.prev').addEventListener('click', function () {
-    slider.goTo('prev');
-  });
-
-  document.querySelector('.next').addEventListener('click', function () {
-    slider.goTo('next');
-  });
-   */
